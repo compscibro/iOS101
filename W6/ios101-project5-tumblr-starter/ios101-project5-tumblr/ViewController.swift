aea6f6c55d2984ab2670e682def07d44f0c7a939
Mohammed Abdur Rahman <mohammed.edu76@gmail.com>
2025-11-02 01:52:36 -0400

    project 5 is up and working

--- W6/ios101-project5-tumblr-starter/ios101-project5-tumblr/ViewController.swift
+++ W6/ios101-project5-tumblr-starter/ios101-project5-tumblr/ViewController.swift
@@ -5,18 +5,41 @@
 
 import UIKit
 import Nuke
+import NukeExtensions
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var posts: [Post] = []   // store decoded posts
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 320
         fetchPosts()
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        let post = posts[indexPath.row]
 
+        cell.summaryLabel.text = post.summary
+
+        if let url = post.photos.first?.originalSize.url {
+            NukeExtensions.loadImage(with: url, into: cell.postImageView)
+        } else {
+            cell.postImageView.image = nil
+        }
+
+        return cell
+    }
 
     func fetchPosts() {
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
@@ -39,15 +62,9 @@ class ViewController: UIViewController {
             do {
                 let blog = try JSONDecoder().decode(Blog.self, from: data)
 
-                DispatchQueue.main.async { [weak self] in
-
-                    let posts = blog.response.posts
-
-
-                    print("✅ We got \(posts.count) posts!")
-                    for post in posts {
-                        print("🍏 Summary: \(post.summary)")
-                    }
+                DispatchQueue.main.async {
+                    self.posts = blog.response.posts
+                    self.tableView.reloadData()
                 }
 
             } catch {
